a056d74bf90f9c3c35041a395f52e085ab0bf232
Avitus <Avitus@Wanee>
2019-10-26 14:40:54 +0900

    두번째 커밋 브런치 test02

--- src/git_test/helloworld.java
+++ src/git_test/helloworld.java
@@ -3,6 +3,6 @@ package git_test;
 public class helloworld {
 	public static void main(String[] args) {
 		System.out.println("hello world");
-		System.out.println("Branch Test1");
+		System.out.println("Branch Test2");
 	}
 }
